3fafe25b15d76804ed5a6af8168d743732789867
Azim Sofi <azimmiskandar@gmail.com>
2023-08-25 10:01:42 +0800

    bootstrap例、登録ページ更新

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,16 +9,13 @@ exports.view = (req, res) => {
 exports.create = (req,res) => {
 	console.log("登録.作成");
 
-	/*Note: when you are passing form obj directly to collection using model you
-			have to give same name in form of that data that is to be passed in database 
-			and that name is declared inside schema*/
-	const user = new User(req.body); //passing object of form data directly to collection
-	user.save() //then saving this to database and this return promise
+	const user = new User(req.body); // 新しいUserインスタンスを作成
+	user.save() // 定義したUserSchema、保存
 	.then((result) => {
-		res.redirect('/home');//is success save this will redirect to home page
+		res.redirect('/home');
 	})
 	.catch(err => {
 		console.log(err);
         res.status(400).send('エラー: ユーザーの保存に失敗しました');
 	});
-	}
+}
